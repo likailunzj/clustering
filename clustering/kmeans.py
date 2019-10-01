e1ce0db7c763143f281ec7cce688b26790ecc993
Kailunli <likailunzj@126.com>
2019-09-30 20:38:57 -0400

    initial commit

--- clustering/kmeans.py
+++ clustering/kmeans.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 from math import inf
 import random
 import csv
-
+import math
 
 def point_avg(points):
     """
@@ -11,17 +11,34 @@ def point_avg(points):
     
     Returns a new point which is the center of all the points.
     """
-    raise NotImplementedError()
+    dimi = len(points[0])
+    total_num = len(points)
+    new_center = []
+    for dim in range(dimi):
+        sum_of_this_dim = 0
+        for pt in points:
+            sum_of_this_dim += pt[dim]
+        ave = sum_of_this_dim/total_num
+        new_center.append(ave)
+    return new_center
 
 
-def update_centers(data_set, assignments):
+
+def update_centers(data_set, assignments, k):
     """
     Accepts a dataset and a list of assignments; the indexes 
     of both lists correspond to each other.
     Compute the center for each of the assigned groups.
     Return `k` centers in a list
     """
-    raise NotImplementedError()
+    new_center_list = []
+    for cluster_num in range(k):
+        list_based_on_labels = []
+        for label_index in range(len(assignments)):
+            if assignments[label_index] == cluster_num:
+                list_based_on_labels.append(data_set[label_index])
+        new_center_list.append(point_avg(list_based_on_labels))
+    return new_center_list
 
 
 def assign_points(data_points, centers):
@@ -44,7 +61,7 @@ def distance(a, b):
     """
     Returns the Euclidean distance between a and b
     """
-    raise NotImplementedError()
+    return math.sqrt((a[0]-b[0])**2+(a[1]-b[1])**2)
 
 
 def generate_k(data_set, k):
@@ -52,11 +69,19 @@ def generate_k(data_set, k):
     Given `data_set`, which is an array of arrays,
     return a random set of k points from the data_set
     """
-    raise NotImplementedError()
+    # shuffled = random.shuffle(data_set, random.random)
+    # return shuffled[:k]
+    return random.choices(data_set, k = k)
+
 
 
 def get_list_from_dataset_file(dataset_file):
-    raise NotImplementedError()
+    data_list = []
+    with open(dataset_file) as csv:
+        for line in csv:
+            indivi_data = [int(line[0]),int(line[1])]
+            data_list.append(indivi_data)
+    return data_list
 
 
 def cost_function(clustering):
@@ -69,10 +94,12 @@ def k_means(dataset_file, k):
     assignments = assign_points(dataset, k_points)
     old_assignments = None
     while assignments != old_assignments:
-        new_centers = update_centers(dataset, assignments)
+        new_centers = update_centers(dataset, assignments, k)
         old_assignments = assignments
         assignments = assign_points(dataset, new_centers)
     clustering = defaultdict(list)
     for assignment, point in zip(assignments, dataset):
         clustering[assignment].append(point)
     return clustering
+
+#result = k_means("/Users/AaronLee/clustering/tests/test_files/dataset_1_k_is_2_1.csv", 2)
